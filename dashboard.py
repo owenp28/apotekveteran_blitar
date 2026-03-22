5e727d40fd0165795d691eda84ddb817e68b8ee7
Owen Prathama Binusian 2027 <145041024+owenp28@users.noreply.github.com>
2026-03-22 13:10:14 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -510,7 +510,9 @@ elif menu == "🛒 Update Stok & Kasir":
                         padding: 16px; border-radius: 8px; max-width: 360px;">
                 <div style="text-align: center; border-bottom: 1px dashed #000; padding-bottom: 10px;">
                     <b style="font-size: 15px;">APOTEK VETERAN SEHAT BLITAR</b><br>
-                    Jl. Veteran no 64B Blitar Kota (Sebelah Gang Srigading), Blitar 66111<br>
+                    Jl. Veteran no 64B Blitar Kota<br> 
+                    (Sebelah Gang Srigading)<br> 
+                    Blitar 66111<br>
                     <b>081331808585</b>
                 </div>
                 <div style="margin: 10px 0; font-size: 12px;">
@@ -593,7 +595,9 @@ elif menu == "🛒 Update Stok & Kasir":
             </head><body>
             <div class="center">
               <b>APOTEK VETERAN SEHAT BLITAR</b><br>
-              Jl. Veteran no 64B Blitar Kota (Sebelah Gang Srigading), Blitar 66111<br>
+              Jl. Veteran no 64B Blitar Kota<br> 
+              (Sebelah Gang Srigading)<br> 
+              Blitar 66111<br>
               <b>081331808585</b>
             </div>
             <div class="dashed"></div>
