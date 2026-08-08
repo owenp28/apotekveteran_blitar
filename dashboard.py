bb64d60bde28b3a96713526a50810e139ccb841d
Owen Prathama Binusian 2027 <145041024+owenp28@users.noreply.github.com>
2026-08-08 11:53:06 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -562,12 +562,12 @@ st.sidebar.markdown("---")
 
 if _role == "Admin":
     _menu_options = [
-        "🏠 Beranda",
-        "📋 Tampilkan Dan Ubah Stok Obat",
-        "🖨️ Cetak & Print Stok Obat",
-        "📦 Entri & Retur Pembelian",
-        "🛒 Kasir Pembelian Obat",
-        "🕒 Buka/Tutup Shift"
+        "🏠 Dashboard",
+        "📋 Kelola Stok",
+        "🖨️ Rekap Data",
+        "📦 Retur & Entry",
+        "🛒 Kasir Pembelian",
+        "🕒 Sesi Shift"
     ]
 else:  
     _menu_options = [
